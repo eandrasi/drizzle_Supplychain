110849427a3f129cfea03f5fbd97c024b60e8342
Eduard Andrasi <eduard.andrasi@devoteam.com>
2019-07-11 21:20:36 +0200

    worked on SupplyChain contract

---
+++ contracts/bloodSupChain_accessControl/AccessControl.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import './DonorRole.sol';
+import './HospitalRole.sol';
+import './PatientRole.sol';
+import './ProcessingCenterRole.sol';
+
+contract AccessControl is DonorRole, HospitalRole, PatientRole, ProcessingCenterRole {
+    constructor() public {}
+}
--- contracts/bloodSupChain_core/Ownable.sol
+++ contracts/bloodSupChain_core/Ownable.sol
@@ -1,7 +1,5 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-import '../bloodSupChain_accessControl/AccessControl.sol';
-
 /// Provides basic authorization control
 contract Ownable {
     address private origOwner;
